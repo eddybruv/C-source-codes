969a3b87f643acf381a73b6cc0154c636b19aaae
eddybruv <edwinajong@gmail.com>
2021-07-18 20:27:48 +0100

    sum intergers in a dynamic 1D interger array

--- 1D_arrays_in_c.c
+++ 1D_arrays_in_c.c
@@ -14,7 +14,7 @@ int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
     int n,count = 0;
     scanf("%d", &n);
-    int *arr = (int*)malloc(n*sizeof(int));
+    int *arr = (int*)malloc(n*sizeof(int));//allocate memory dynamically
     for(int i = 0;i < n;i++){
         scanf("%d",&arr[i]);
         count += arr[i];
