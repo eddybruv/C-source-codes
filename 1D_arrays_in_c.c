26340d16150c5baf12aada754dc8dfdeaeec00bc
eddybruv <edwinajong@gmail.com>
2021-07-18 20:26:45 +0100

    sum all intergers in a dyanamic 1D array

---
+++ 1D_arrays_in_c.c
@@ -0,0 +1,25 @@
+/*Date: 18-07-2021
+    by eddybruv
+    exercise from hackerrank.com
+*/
+
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int n,count = 0;
+    scanf("%d", &n);
+    int *arr = (int*)malloc(n*sizeof(int));
+    for(int i = 0;i < n;i++){
+        scanf("%d",&arr[i]);
+        count += arr[i];
+    }        
+    
+    printf("%d",count);
+    return 0;
+}
