4dcf08ef67225e435866573697bb44c89d74388b
eddybruv <edwinajong@gmail.com>
2021-07-22 03:43:37 +0100

    returns the highest divisor (0 - 9) of a number

---
+++ div.c
@@ -0,0 +1,20 @@
+/*Date: 22-07-2021
+	by eddybruv
+	exercise from codechef
+	*/
+
+
+#include <stdio.h>
+
+int main(void) {
+	float num;
+	scanf("%f", &num);
+	float highest_div;
+	for(int i = 0; i < 11; i++){
+	    if(((int)num % i) == 0)
+	        highest_div = i;
+            break;
+	}
+	printf("%d", (int)highest_div);
+	return 0;
+}
