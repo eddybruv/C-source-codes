c1136de1764296b3c36775bc2d191b52dcde7d49
Edwin B. Ajong <edwinajong@gmail.com>
2021-09-13 19:43:54 +0100

    fixed comments

--- div.c
+++ div.c
@@ -1,19 +1,21 @@
-/*Date: 22-07-2021
-	by eddybruv
-	exercise from codechef
-	*/
-
+/**
+ * Date: 22-07-2021
+ * by eddybruv
+ * exercise from codechef
+**/
 
 #include <stdio.h>
 
-int main(void) {
+int main(void)
+{
 	float num;
 	scanf("%f", &num);
 	float highest_div;
-	for(int i = 0; i < 11; i++){
-	    if(((int)num % i) == 0)
-	        highest_div = i;
-            break;
+	for (int i = 0; i < 11; i++)
+	{
+		if (((int)num % i) == 0)
+			highest_div = i;
+		break;
 	}
 	printf("%d", (int)highest_div);
 	return 0;
