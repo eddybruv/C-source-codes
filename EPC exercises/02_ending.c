26c33b1a6b13ebd9b22aa24092a3caf28b16ca9f
eddybruv <edwinajong@gmail.com>
2021-10-06 19:49:25 +0100

    challenge on string ending

--- EPC exercises/02_ending.c
+++ EPC exercises/02_ending.c
@@ -6,10 +6,26 @@
 #include <string.h>
 #include <stdbool.h>
 
-bool confirmEnding(char *arr, char terminal)
+bool confirmEnding(const char *str,const char *target)
 {
-    int len = strlen(arr);
-    if(arr[len] == terminal)
-        return true;
-    return false;
+    int targetLength = strlen(target);
+    int strLength = strlen(str);
+
+    if(strLength < targetLength){
+        return false;
+    }
+
+    int posOfStr = strLength - targetLength;
+    int posOfTarget = 0;
+
+    while(str[posOfStr] != '\0'){
+        if(str[posOfStr] != target[posOfTarget]){
+            return false;
+        }
+
+        posOfStr++;
+        posOfTarget++;
+    }
+
+    return true;
 }
